444b4d2fa709f2b8dfe2ff64b2dc0e3756494ea8
Soham Khade <sohamkhade@Sohams-MacBook-Pro.local>
2021-02-14 20:31:45 +0530

    gallery animation done

--- src/Components/Gallery/gallery.js
+++ src/Components/Gallery/gallery.js
@@ -7,7 +7,7 @@ function gallery() {
     return (
         <section>
             <article className="cf">
-                <div className="fl w-50 w-25-ns pa3">
+                <div className="fl w-50 w-25-ns pa3" data-aos={'fade-down'} data-aos-duration={'2000'}>
                     <a href="https://www.amazon.com/BEYONCÉ-Beyonce/dp/B00KCOMBJC/ref=sr_1_2_twi_lp__3?s=music&ie=UTF8&qid=1480422067&sr=1-2&keywords=beyonce&tag=mrmrs01-20"
                        className="db aspect-ratio aspect-ratio--1x1 dim">
                         <span role="img" aria-label="Beyoncé"
@@ -15,7 +15,7 @@ function gallery() {
                               className="bg-center cover aspect-ratio--object"></span>
                     </a>
                 </div>
-                <div className="fl w-50 w-25-ns pa3">
+                <div className="fl w-50 w-25-ns pa3" data-aos={'fade-down'} data-aos-duration={'2000'}>
                     <a href="https://www.amazon.com/99-9-KAYTRANADA/dp/B01D9DBNX2/ref=sr_1_1_twi_lp__3?s=music&ie=UTF8&qid=1480422105&sr=1-1&keywords=kaytranada+vinyl&tag=mrmrs01-20"
                        className="db aspect-ratio aspect-ratio--1x1 dim">
                         <span role="img" aria-label="Kaytranada"
@@ -23,7 +23,7 @@ function gallery() {
                               className="bg-center cover aspect-ratio--object"></span>
                     </a>
                 </div>
-                <div className="fl w-50 w-25-ns pa3">
+                <div className="fl w-50 w-25-ns pa3" data-aos={'fade-down'} data-aos-duration={'2000'}>
                     <a href="https://www.amazon.com/Woman-2LP-Set-Full-Album/dp/B01LX3E0ET/ref=sr_1_1?s=music&ie=UTF8&qid=1480422119&sr=1-1&keywords=justice&tag=mrmrs01-20"
                        className="db aspect-ratio aspect-ratio--1x1 dim">
                         <span role="img" aria-label="Woman - Justice"
@@ -31,7 +31,7 @@ function gallery() {
                               className="bg-center cover aspect-ratio--object"></span>
                     </a>
                 </div>
-                <div className="fl w-50 w-25-ns pa3">
+                <div className="fl w-50 w-25-ns pa3" data-aos={'fade-down'} data-aos-duration={'2000'}>
                     <a href="https://www.amazon.com/Skin-Flume/dp/B01DD5N35W/ref=sr_1_1_twi_lp__3?s=music&ie=UTF8&qid=1480422133&sr=1-1&keywords=flume&tag=mrmrs01-20"
                        className="db aspect-ratio aspect-ratio--1x1 dim">
                         <span role="img" aria-label="Skin - Flume"
@@ -39,7 +39,7 @@ function gallery() {
                               className="bg-center cover aspect-ratio--object"></span>
                     </a>
                 </div>
-                <div className="fl w-50 pa3">
+                <div className="fl w-50 pa3" data-aos={'fade-right'} data-aos-duration={'2000'}>
                     <a href="https://www.amazon.com/Seat-at-Table-Solange/dp/B01LXP7I5N/ref=sr_tnr_p_1_195429011_1_twi_lp__3?s=music&ie=UTF8&qid=1480422087&sr=1-1&keywords=solange+seat+at+the+table&tag=mrmrs01-20"
                        className="db aspect-ratio aspect-ratio--1x1 dim">
                         <span role="img" aria-label="Seat at Table Solange"
@@ -47,7 +47,7 @@ function gallery() {
                               className="bg-center cover aspect-ratio--object"></span>
                     </a>
                 </div>
-                <div className="fl w-50 w-25-ns pa3">
+                <div className="fl w-50 w-25-ns pa3" data-aos={'flip-up'} data-aos-duration={'2000'}>
                     <a href="https://www.amazon.com/untitled-unmastered-LP-Kendrick-Lamar/dp/B01DET9BV2/ref=sr_1_3_twi_lp__3?s=music&ie=UTF8&qid=1480370912&sr=1-3&keywords=kendrick+lamar&tag=mrmrs01-20"
                        className="db aspect-ratio aspect-ratio--1x1 dim">
                         <span role="img" aria-label="Untitled Unmastered - Kendrick Lamar"
@@ -55,7 +55,7 @@ function gallery() {
                               className="bg-center cover aspect-ratio--object"></span>
                     </a>
                 </div>
-                <div className="fl w-100 w-25-ns pa3">
+                <div className="fl w-100 w-25-ns pa3" data-aos={'fade-left'} data-aos-duration={'2000'}>
                     <a href="https://www.amazon.com/Moon-Shaped-Pool-2-LP-Download/dp/B01FDF12UI/ref=sr_1_1_twi_lp__3?s=music&ie=UTF8&qid=1480370971&sr=1-1&keywords=moon+shaped+pool&tag=mrmrs01-20"
                        className="db aspect-ratio aspect-ratio--1x1 dim">
                         <span role="img" aria-label="Moon Shaped Pool 2"
@@ -63,7 +63,7 @@ function gallery() {
                               className="bg-center cover aspect-ratio--object"></span>
                     </a>
                 </div>
-                <div className="fl w-50 w-25-ns pa3">
+                <div className="fl w-50 w-25-ns pa3" data-aos={'flip-down'} data-aos-duration={'2000'}>
                     <a href="https://www.amazon.com/Colour-Anything-2-LP/dp/B01F8674B8/ref=sr_1_1_twi_lp__3?s=music&ie=UTF8&qid=1480371036&sr=1-1&keywords=color+in+anything&tag=mrmrs01-20"
                        className="db aspect-ratio aspect-ratio--1x1 dim">
                         <span role="img" aria-label="Colour Anything 2"
@@ -71,7 +71,7 @@ function gallery() {
                               className="bg-center cover aspect-ratio--object"></span>
                     </a>
                 </div>
-                <div className="fl w-50 w-25-ns pa3">
+                <div className="fl w-50 w-25-ns pa3" data-aos={'fade-left'} data-aos-duration={'2000'}>
                     <a href="https://www.amazon.com/Good-Luck-Do-Your-Best/dp/B01C3IHINI/ref=sr_1_1_twi_lp__3?s=music&ie=UTF8&qid=1480371123&sr=1-1&keywords=good+luck+gold+panda&tag=mrmrs01-20"
                        className="db aspect-ratio aspect-ratio--1x1 dim">
                         <span role="img" aria-label="Good Luck Do Your Best"
