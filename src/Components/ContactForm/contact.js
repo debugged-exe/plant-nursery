5e310e9482f14ec689c0ab50738ea59dbd0f88be
tanmayjagtap <tanmayjagtap27@gmail.com>
2021-02-14 14:39:40 +0530

    Added Contact Parallex Effect

--- src/Components/ContactForm/contact.js
+++ src/Components/ContactForm/contact.js
@@ -1,56 +1,30 @@
 import React from "react";
-import imageUrl1 from "./question.png";
 import 'tachyons';
+import './contact.css'
 function Contact() {
     return (
-        <section id={'section-contact'}>
-            <div className="mw9 center ph3-ns ">
-                <div className={"tc"}>
-                    <h1>Have Some Questions</h1>
+        <div>
+        <div className='margin'></div>
+        <section className='parallax'>
+            <div className='parallax-inner flex flex-column justify-center items-center'>
+                <h1>Contact Us</h1>
+                <div className='mv2'>
+                    <p>Name</p>
+                    <input className='bg-none br2' placeholder='Enter Name'/>
+                </div >
+                <div className='mv2'>
+                    <p>Email</p>
+                    <input className='bg-none br2' placeholder='Enter Email'/>
                 </div>
-                <div className="cf ph2-ns ">
-                    <div className="fl  w-50-ns pa2">
-                        <img src={imageUrl1} alt="question"/>
-                    </div>
-                    <div className="fl w-50-ns  pa2">
-                        <form className="flex items-center flex-column tc mv6-l pl2-m " style={{width: '100%'}}>
-                            <input
-                                type="text"
-                                className={"w-100 h2 mt2"}
-                                name="fname"
-                                placeholder="Full Name"
-                            />
-                            <br/>
-                            <input
-                                type="email"
-                                className={"w-100 h2 mt2 "}
-                                name="email"
-                                placeholder="Email"
-                            />
-                            <br/>
-                            <input
-                                type="tel"
-                                className={"w-100 h2 mt2"}
-                                name="Ph"
-                                placeholder="Phone Number"
-                            />
-                            <br/>
-                            <textarea
-                                rows={"2"}
-                                className={"w-100 h3 mt2 f4"}
-                                placeholder={"Message"}
-                            ></textarea>
-                            <a
-                                className="f6 link dim br2 ph3 pv2 mb2 dib white bg-dark-green mt1"
-                                href="#0"
-                            >
-                                Submit
-                            </a>
-                        </form>
-                    </div>
+                <div className='mv2'>
+                    <p>Message</p>
+                    <textarea className='bg-none br2 pr4' placeholder='Enter Email'/>
                 </div>
+                <a class="mt4 br2 grow f4 link ph3 pv2 mb2 dib white bg-dark-green" href="#0">Send Query</a>
             </div>
         </section>
+        <div className='margin'></div>
+    </div>
     );
 }
 
