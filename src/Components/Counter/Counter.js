55e128ec7eefc19eb38d8119a98bc365ee1bd649
sumedhovhal <sumedhovhal765@gmail.com>
2021-02-21 13:49:52 +0530

    counter changes

--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -1,15 +1,15 @@
 import React, {useState} from 'react';
 import CountUp from 'react-countup';
-import {FaSeedling} from 'react-icons/fa';
+import {RiSeedlingLine} from 'react-icons/ri';
 import {RiPlantFill} from 'react-icons/ri';
-import {GiApothecary} from 'react-icons/gi';
+import {GiFlowerPot} from 'react-icons/gi';
 import {FaTools} from 'react-icons/fa';
 import './counterUp.css';
 const Counter = () => {
     return (
         <section className="f3 flex flex-wrap justify-around items-center mt4 ">
             <div data-aos={'fade-right'} data-aos-duration={'2000'} className={'pointer flex flex-column justify-center items-center ba br2 pa4 ph5 mh2 mv2 grow shadow-2 transparent-bg'}>
-                <FaSeedling size={"2.4rem"} className={'pa0 ma0'}/>
+                <RiSeedlingLine size={"2.4rem"} className={'pa0 ma0'}/>
                 <p className={'pa0 ma0'}><CountUp end={50} duration={20}/></p>
                 <p className={'pa0 ma0'}>Seeds</p>
             </div>
@@ -19,7 +19,7 @@ const Counter = () => {
                 <p className={'pa0 ma0'}>Plants</p>
             </div>
             <div data-aos={'fade-left'} data-aos-duration={'1000'} className={'pointer flex flex-column justify-center items-center ba br2 pa4 ph5 mh2 mv2 grow shadow-2 transparent-bg'}>
-                <GiApothecary size={"2.4rem"} className={'pa0 ma0'}/>
+                <GiFlowerPot size={"2.4rem"} className={'pa0 ma0'}/>
                 <p className={'pa0 ma0'}><CountUp end={53} duration={20} /></p>
                 <p className={'pa0 ma0'}>Pots</p>
             </div>
