bb5e5f4c6a351e30c64dffb35f0dadb976c0027d
Soham Khade <sohamkhade@Sohams-MacBook-Pro.local>
2021-02-25 16:41:57 +0530

    changes made

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -57,18 +57,9 @@ function Footer() {
                         </ul>
                     </li>
                     <li>
-<<<<<<< HEAD
                         <h2 className={'h2'}><a
                             href="https://www.google.com/maps/place/Rohit+Rose+Nursery/@18.1946358,74.7885891,17.44z/data=!4m5!3m4!1s0x3bc379cda998a349:0x8794d03056af9de3!8m2!3d18.1940078!4d74.790571"
                             target="_blank">Address</a></h2>
-=======
-                        <h2 className={'h2'}
-                         href="https://www.google.com/maps/place/Rohit+Rose+Nursery/@18.1946358,74.7885891,17.44z/data=!4m5!3m4!1s0x3bc379cda998a349:0x8794d03056af9de3!8m2!3d18.1940078!4d74.790571"
-                         target="_blank"
-                        >
-                            Address
-                        </h2>
->>>>>>> d04bee0ec148cfcc2f50b873b50fe1bbdd750e34
                         <ul className="box">
                             <li><a
                                 href="https://www.google.com/maps/place/Rohit+Rose+Nursery/@18.1946358,74.7885891,17.44z/data=!4m5!3m4!1s0x3bc379cda998a349:0x8794d03056af9de3!8m2!3d18.1940078!4d74.790571"
