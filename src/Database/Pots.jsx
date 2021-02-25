fb3f1ac032e6c4eb47bfc3f916b6340f068aa084
Soham Khade <sohamkhade@Sohams-MacBook-Pro.local>
2021-02-25 13:42:15 +0530

    pot and tool images added

--- src/Database/Pots.jsx
+++ src/Database/Pots.jsx
@@ -1,53 +1,58 @@
-import imgurl1 from './Images/plant1.png';
-import imgurl2 from './Images/plant2.png';
-import imgurl3 from './Images/plant3.png';
-import imgurl4 from './Images/plant4.png';
+import FootballCeramicPot from './Images/PotImages/Football Ceramic Pot.jpg';
+import AppleCeramicPot from './Images/PotImages/Apple Ceramic Pot.jpg';
+import AristoRainbowPlanter from './Images/PotImages/aristo pot.jpg';
+import WatermelonCeramicPlanter from './Images/PotImages/watermelon cermic pot.jpg';
+import HandiCeramicPot from './Images/PotImages/Handi ceramic pot.jpg';
+import SquareCeramicPot from './Images/PotImages/Square ceramic pot.jpg';
+import TuskerCeramicPot from './Images/PotImages/Tusker ceramic pot.jpg';
+import PipeCeramicPot from './Images/PotImages/pipe ceramic pot.jpg';
+import StarCeramicPot from './Images/PotImages/Star ceramic pot.jpg';
 const pots = [
     {
         name: "Football Ceramic Pot (5.3 Inch Diameter)",
         desc:"Etch your home decor with the Football Planter and leave your guests pondering over your distinct taste and flair for unique objects. With this planter, you can now enhance your charming and aromatic plants and foliage.",
-        imgUrl: imgurl1
+        imgUrl: FootballCeramicPot
     },
     {
         name: "Apple Ceramic Pot (6.6 Inch Diameter)",
         desc: "A lovely plant in an edible shape is bound to delight the spectators. Crafted for those who love only the extraordinary, this apple shaped pot is a beauty. Your kids would love to grow their favourite veggie into these. Won't they?",
-        imgUrl: imgurl2
+        imgUrl: AppleCeramicPot
     },
     {
-        name: "Apple Ceramic Pot (3.8 Inch Diameter)",
-        desc: "A lovely plant in an edible shape is bound to delight the spectators. Crafted for those who love only the extraordinary, this yellow apple shaped pot is a beauty. Your kids would love to grow their favourite veggie into these. Won't they?",
-        imgUrl: imgurl3
+        name: "Aristo Rainbow Planter",
+        desc: "This lovely planter will grace your garden arena in style. Meticulously designed it looks beautiful in the terra-cotta color. If you wish to plant directly into this, do not forget to drill a hole at the bottom of the pot. It comes in the following size 13.5 cm height, 15.5 cm width, and 13.5 cm depth.",
+        imgUrl: AristoRainbowPlanter
     },
     
     {
         name: "Watermelon Ceramic Planter (5.5 Inch Width)",
         desc: "Bring a slice of refreshment and splash of soothing colours with this Watermelon pot. Whatever you plant in these would look gorgeous and will make the heads turn for sure. Beware! People would love to savour the Ceramic fruit.",
-        imgUrl: imgurl4
+        imgUrl: WatermelonCeramicPlanter
     },
     {
         name: "Handi Ceramic Pot (4 Inch Diameter)",
         desc: "The Handi pot are some of the most versatile planters available in our entire range of planters. Pair these with your favorite plants, herbs, fruits, and vegetable to create that trendy and cool appearance.",
-        imgUrl: imgurl4
+        imgUrl: HandiCeramicPot
     },
     {
         name: "Square Ceramic Pot (3.5 Inch Width)",
         desc: "Suitable for direct potting, these lovely planters are trendy and will never go out of fashion. You can simply add these to your decor and give a tidy look to your patio, entrance, balcony, and porch.",
-        imgUrl: imgurl4
+        imgUrl: SquareCeramicPot
     },
     {
         name: "Tusker Ceramic Pot (4 Inch Diameter)",
         desc: "Pamper your garden with this lovely Ceramic tusker pot. Elegant and full of allure the beautiful planter has been crafted specially for your favourite floral and gorgeous indoor plants.",
-        imgUrl: imgurl4
+        imgUrl: TuskerCeramicPot
     },
     {
         name: "Pipe Ceramic Pot (3.8 Inch Diameter)",
         desc: "Special plants deserve a special place. Hence we have crafted this lovely Ceramic pot for you. Place your favourite plant into this and let it flourish with pride.",
-        imgUrl: imgurl4
+        imgUrl: PipeCeramicPot
     },
     {
         name: "Star Ceramic Pot (4.2 Inch Width)",
         desc: "Break free from the monotonous shapes and add pots with unique and never-Sean-before form in your garden. Bring up your favourite plant in this vibrant and attractive orange star shaped pot and let people just peek into your garden for inspiration.",
-        imgUrl: imgurl4
+        imgUrl: StarCeramicPot
     }
 ];
 
--- src/Database/Tools.jsx
+++ src/Database/Tools.jsx
@@ -1,55 +1,59 @@
-import imgurl1 from './Images/plant1.png';
-import imgurl2 from './Images/plant2.png';
-import imgurl3 from './Images/plant3.png';
-import imgurl4 from './Images/plant4.png';
+import PyramidPlasticImpulseSprinkler from './Images/ToolImages/pyramid plastic impulse sprinkler.jpg';
+import HandPressureSprayerPump from './Images/ToolImages/hand pressure sprayer pump.jpg';
+import PyramidPrecisionPruner from './Images/ToolImages/pyramid precision pruner.jpg';
+import FalconSingleProngWeeder from './Images/ToolImages/falcon single prong weeder with plastic handle.jpg';
+import FalconSecateurSuperType from './Images/ToolImages/falcon secateur super type.jpg';
+import PyramidLongHandleCultivator from './Images/ToolImages/pyramid long handle cultivator.jfif';
+import PyramidSingleEdgeSickle from './Images/ToolImages/Pyramid Single-Edge Sickle.jpg';
+import PyramidDoubleSideRakeTrowel from './Images/ToolImages/pyramid double side rake and trowel.jpg';
+import PyramidTrowelWithWoodenHandleAndCushionGrip from './Images/ToolImages/pyramid trowel with wooden handle and cushion grip.jfif';
 const tools = [
     {
         name: "Pyramid Plastic Impulse Sprinkler",
         desc:"The pulsating sprinkler head adjusts conveniently from partial to full circle. It provides full uniform coverage up to 60 feet in diameter and is effective for long-distance sprinkling.",
-        imgUrl: imgurl1
+        imgUrl: PyramidPlasticImpulseSprinkler
     },
     {
         name: "Hand Pressure Sprayer Pump - 3 Ltr",
         desc: "Irrigate your small garden effectively with the Hand Pressure Sprayer",
-        imgUrl: imgurl2
+        imgUrl: HandPressureSprayerPump
     },
     {
         name: "Pyramid Precision Pruner",
         desc: "The Pyramid Precision Pruner is a small and durable tool that is mainly used for Bonsai tree cutting or for cutting plants. The very useful narrow SK5 Japanese blades of this secateur cut very smoothly. It has a fixed blade with a hard PVC grip.",
-        imgUrl: imgurl3
+        imgUrl: PyramidPrecisionPruner
     },
     
     {
         name: "Falcon Single Prong Weeder with Plastic Handle",
         desc: "The Falcon Single Prong Weeder with Plastic Handle is an essential tool used by every gardener for removing weeds and also o dig holes to sow seeds. It is perfectly sized for flower beds and small vegetable gardens. It comes with a plastic handle for a powerful and firm grip.",
-        imgUrl: imgurl4
+        imgUrl: FalconSingleProngWeeder
     },
     {
         name: "Falcon Secateur Super Type",
         desc: "The Falcon Secateur Super Type feature with hardened steel blades which powers through the branches with ease and is equipped with a plastic grip for a comfortable and secure hold. It is ideal for optimal performance season after season in eliminating twigs and branches.",
-        imgUrl: imgurl4
+        imgUrl: FalconSecateurSuperType
     },
     {
         name: "Pyramid Long Handle Cultivator",
         desc: "The Pyramid Long Handle Cultivator is a useful lightweight tool for aerating and loosening soil. It is fitted with a body of polished steel and a plastic handle for a comfortable and adjustable grip. The extended handle allows for a greater reach in your garden.",
-        imgUrl: imgurl4
+        imgUrl: PyramidLongHandleCultivator
     },
     {
-        name: "Falcon Secateur Super Type",
-        desc: "The Falcon Secateur Super Type feature with hardened steel blades which powers through the branches with ease and is equipped with a plastic grip for a comfortable and secure hold. It is ideal for optimal performance season after season in eliminating twigs and branches.",
-        imgUrl: imgurl4
+        name: "Pyramid Single-Edge Sickle",
+        desc: "The Pyramid Single-Edge Sickle is one of the most ancient of harvesting tools. It is a superior quality tool used to cut grass or harvest crops very easily. It features a wooden handle which is provided for a firm grip and allows the user to complete their task with speed and efficiency.",
+        imgUrl: PyramidSingleEdgeSickle
     },
     {
         name: "Pyramid Double Side Rake & Trowel",
         desc: "The Pyramid Double Side Rake & Trowel is highly beneficial for your garden. It clears leaves, grass, and other debris with the rake on one side and digs, scoops, and moves dirt with the Trowel the other side. It has a body of green-coated polished steel and a plastic handle for a comfortable and firm grip.",
-        imgUrl: imgurl4
+        imgUrl: PyramidDoubleSideRakeTrowel
     },
     {
         name: "Pyramid Trowel with Wooden Handle and Cushion Grip",
         desc: "This Pyramid Trowel is the perfect tool to reduce hand fatigue while digging in the garden. It features a cushioned, wooden handle that provides excellent comfort and control, and the rust-resistant, extra-large powder-coated steel blade features a sharpened black edge to cut into the ground with ease.",
-        imgUrl: imgurl4
+        imgUrl: PyramidTrowelWithWoodenHandleAndCushionGrip
     }
-
 ];
 
 export default tools;
